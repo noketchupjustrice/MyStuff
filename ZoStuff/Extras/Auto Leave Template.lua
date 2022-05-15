12e5745e64d3036a6d07463f97b4715f1831d551
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-14 21:10:09 -0700

    Create Auto Leave Template.lua

---
+++ ZoStuff/Extras/Auto Leave Template.lua
@@ -0,0 +1,6 @@
+--You Can Choose To Loop This Or Not Although It Is Preferred Just Incase They Join
+--while true do
+if game.Players.FullUsername then -- Insert Full Username (Not Display) Here | Case Sensitive Has To Be Exact
+    game.Players.LocalPlayer:Kick("User Is In Or Has Joined The Game") --Customize The Message However You'd Like\
+-- You Can Also Use game:Shutdown() If You Do Not Want A Custom Message And Just Want To Instantly Close The Client
+end
