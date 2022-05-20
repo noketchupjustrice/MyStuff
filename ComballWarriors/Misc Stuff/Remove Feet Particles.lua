b6e1fbd1182a18139d621269c7f96ab30ae95976
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-19 17:16:38 -0700

    Create Remove Feet Particles.lua

---
+++ ComballWarriors/Misc Stuff/Remove Feet Particles.lua
@@ -0,0 +1,3 @@
+--Remove Feet Particles 
+game.Players.LocalPlayer.Character.HumanoidRootPart.FootLandParticlesPart:Destroy() --Remote Jumping Particles
+game.Players.LocalPlayer.Character.HumanoidRootPart.SprintingParticlesPart:Destroy() --Sprinting/Sliding Particles
