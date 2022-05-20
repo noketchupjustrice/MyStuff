63ba6730e9d9365106a115cf23b39a6fef3cff8e
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-19 17:04:48 -0700

    Create Walk On Water.lua

---
+++ ComballWarriors/Misc Stuff/Walk On Water.lua
@@ -0,0 +1,6 @@
+--Walk On Water 
+--On
+game:GetService("Workspace").Map.Crossroads.MapConfiguration.WaterAreas.WaterArea.CanCollide = true --Able To Colide With Water Part 
+
+--Off
+game:GetService("Workspace").Map.Crossroads.MapConfiguration.WaterAreas.WaterArea.CanCollide = false --Not Able To Colide With Water Part
