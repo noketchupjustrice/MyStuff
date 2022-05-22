88cddb0c8a9f5f15b05a20abf483aa6e793ffda1
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-22 01:27:08 -0700

    Update and rename JumpCooldown.lua to Disablers.lua

--- Mortem Metallum/Disablers.lua
+++ Mortem Metallum/Disablers.lua
@@ -1,3 +1,11 @@
+--Jumpcooldown
 game.Players.LocalPlayer.Character.JumpCooldown.Disabled = true --Disables The Cooldown Script Which Works :3
 game.Players.LocalPlayer.Character.JumpCooldown.Disabled = false --Turns On The Cooldown
 game.Players.LocalPlayer.Character.JumpCooldown:Destroy() -- Destroying It Will Not Work Because It Gets Detected And Will Just Crash Your Game
+
+--Ragdoll 
+game.Players.LocalPlayer.Character.Fall.Disabled = true --Disabler For Ragdoll
+game.Players.LocalPlayer.Character.Fall.Disabled = false --Turns On Ragdoll
+game.Players.LocalPlayer.Character.Fall:Destroy() -- Will Just Crash Your Game
+
+
