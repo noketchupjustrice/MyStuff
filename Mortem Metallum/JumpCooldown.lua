dd1944f7402606bdbf4570f35c29981975715828
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-22 01:22:34 -0700

    Update JumpCooldown.lua

--- Mortem Metallum/JumpCooldown.lua
+++ Mortem Metallum/JumpCooldown.lua
@@ -1,3 +1,3 @@
-game.Players.LocalPlayer.Character.JumpCooldown.Disabled = true --Disables The Cooldown Script Which Works 
-
+game.Players.LocalPlayer.Character.JumpCooldown.Disabled = true --Disables The Cooldown Script Which Works :3
+game.Players.LocalPlayer.Character.JumpCooldown.Disabled = false --Turns On The Cooldown
 game.Players.LocalPlayer.Character.JumpCooldown:Destroy() -- Destroying It Will Not Work Because It Gets Detected And Will Just Crash Your Game
