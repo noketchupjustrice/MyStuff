140326fb7c2bfd0e397701926ecd53bbb83829d8
Syskey <101951687+noketchupjustrice@users.noreply.github.com>
2022-05-22 00:51:58 -0700

    Update CoolDownSpoof.lua

--- Mortem Metallum/CoolDownSpoof.lua
+++ Mortem Metallum/CoolDownSpoof.lua
@@ -4,7 +4,7 @@ old_index = mt.__index
 mt.__index = function(a, b)
     if tostring(a) == "cooldown" then
         if tostring(b) == "Value" then
-            return false
+            return false --Turns CoolDown Off So You Can Infinitely Roll Or Something Idk Lol :3
         end
     end
     return old_index(a, b)
