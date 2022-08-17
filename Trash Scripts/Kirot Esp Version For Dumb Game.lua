4c231feb2e16c50cb0e0c98c4cd21b2b5fc00ef0
Protocol <101951687+noketchupjustrice@users.noreply.github.com>
2022-08-17 00:49:30 -0700

    Update Kirot Esp Version For Dumb Game.lua

--- Trash Scripts/Kirot Esp Version For Dumb Game.lua
+++ Trash Scripts/Kirot Esp Version For Dumb Game.lua
@@ -224,7 +224,7 @@ function boxBase:Update()
         
         if Vis5 then
             self.Components.Name.Visible = true
-            self.Components.Name.Position = Vector2.new(TagPos.X, TagPos.Y + 10)
+            self.Components.Name.Position = Vector2.new(TagPos.X, TagPos.Y - 10)
             self.Components.Name.Text = self.Name
             self.Components.Name.Color = color
             
